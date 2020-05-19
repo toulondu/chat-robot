9c458b53cbdc652026d3f352ae1d7c1269aedaae
Toulon <420615326@qq.com>
2020-05-19 18:10:53 +0800

    bad performance, wait to be optimized

--- model.py
+++ model.py
@@ -233,9 +233,9 @@ def trimRareWords(voc, pairs, MIN_COUNT=MIN_COUNT):
     return keep_pairs
 
 paris_path = './pairs.json'
+save_dir = os.path.join("data", "save")
 if not os.path.exists(paris_path):
     # 加载/组装voc和对
-    save_dir = os.path.join("data", "save")
     pairs = loadPrepareData(corpus, corpus_name, datafile, save_dir)
 
     # 修剪voc和对
@@ -285,7 +285,7 @@ def outputVar(l, voc):
     max_target_len = max([len(indexes) for indexes in indexes_batch])
     padList = zeroPadding(indexes_batch)
     mask = binaryMatrix(padList)
-    mask = torch.ByteTensor(mask)
+    mask = torch.BoolTensor(mask)
     padVar = torch.LongTensor(padList)
     return padVar, mask, max_target_len
 
@@ -313,13 +313,13 @@ small_batch_size = 5
 batches = batch2TrainData(voc, [random.choice(pairs) for _ in range(small_batch_size)])
 input_variable, lengths, target_variable, mask, max_target_len = batches
 
-print("input_variable:", input_variable)
-print("lengths:", lengths)
-print("target_variable:", target_variable)
-print("mask:", mask)
-print("max_target_len:", max_target_len)
+# print("input_variable:", input_variable)
+# print("lengths:", lengths)
+# print("target_variable:", target_variable)
+# print("mask:", mask)
+# print("max_target_len:", max_target_len)
 
-# 我们这里选择seq2seq作为生成模型，所以我们接下来对 encoder和decoder进行编码 todo..
+# 我们这里选择seq2seq作为生成模型，在解码器中使用attention来利用编码器产生的输出。所以我们接下来对 encoder和decoder进行编码
 # version1:简单点，使用双向RNN，gate采用GRU
 
 # encoder
@@ -339,7 +339,7 @@ class EncoderRNN(nn.Module):
     def forward(self, input_seq, input_lengths, hidden=None):
         # 将单词索引转换为词向量
         embedded = self.embedding(input_seq)
-        # 为RNN模块打包填充batch序列，第一个参数要求我们已经满足:需从长到段排列，形状[max_length,batch_size,*],具体请查看API
+        # 为RNN模块打包填充batch序列，第一个参数要求我们已经满足:需从长到短排列，形状[max_length,batch_size,*],具体请查看API
         # 注意这个打包的作用，因为短句在填充后有大量的pad标记，比如'no way pad pad pad pad',而我们在输入时序模型时，并不希望在单词'way'的隐藏输出h('way')得到之后，再继续对后面的4个pad进行后续RNN运算，而是直接将h('way')作为这个句子的最终表示
         # 这个打包就是用在这里的，将打包后的结果传递给RNN，GRU,LSTM等模型，模型就知道在何处得到输出，而不会再对填充数据进行运算。
         # 另外，打包后得到的数据为PackedSequence类型，需要用pad_packed_sequence解压回tensor
@@ -348,9 +348,419 @@ class EncoderRNN(nn.Module):
         outputs, hidden = self.gru(packed, hidden)
         # 打开填充
         outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs)
-        # 总和双向GRU输出，双向GRU的输出是把正向和反向的输出concat起来的。 我们则将它们取出相加
+        # 综合双向GRU输出，双向GRU的输出是把正向和反向的输出concat起来的。 我们则将它们取出相加
         outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:]
         # 返回输出和最终隐藏状态
         return outputs, hidden
 
 # decoder
+"""
+在没有attention之前，seq2seq结构的生成式任务的编码器只有一个输出，解码器利用这一个输出作为上下文信息，和自身上一步的输出计算当前步骤的输出。
+而这种用单点存储全部上下文信息的方式局限性较大，在句子长度增加时，输入句子前面部分的信息在最后的输出中很难得到体现。
+attention 机制，简单的说，则是保留编码器每一步的输出，然后在解码器的每一步，用一个FC层计算解码器当前隐藏状态与编码器各步骤输出的"注意力(attention)"。
+然后用这个注意力计算出编码器输出的加权和，再与当前隐藏状态计算出当前步骤输出。直觉就是当前输出应该将注意力放在输入句子的哪些词上面。
+attention如今在NLP的应用非常广泛，不仅产生了self-attention,add-attention等变体，在后续的transformer，现在当红的BERT/GPT2等中都发挥了重要的作用。
+"""
+# Luong的attention layer，先定义attention结构
+class Attn(torch.nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        self.method = method
+        if self.method not in ['dot', 'general', 'concat']:
+            raise ValueError(self.method, "is not an appropriate attention method.")
+        self.hidden_size = hidden_size
+        if self.method == 'general':
+            self.attn = torch.nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == 'concat':
+            self.attn = torch.nn.Linear(self.hidden_size * 2, hidden_size)
+            # 创建一个长度为hidden_size的可训练向量，并绑定到Attn层中
+            self.v = torch.nn.Parameter(torch.FloatTensor(hidden_size))
+
+    def dot_score(self, hidden, encoder_output):
+        return torch.sum(hidden * encoder_output, dim=2)
+
+    def general_score(self, hidden, encoder_output):
+        energy = self.attn(encoder_output)
+        # 计算当前隐藏层状态与编码器所有输出的element-wise product(利用广播)，然后对最后一个维度求和，用于后续softmax计算输入句子各部分权重
+        return torch.sum(hidden * energy, dim=2)
+
+    def concat_score(self, hidden, encoder_output):
+        energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)).tanh()
+        return torch.sum(self.v * energy, dim=2)
+
+    def forward(self, hidden, encoder_outputs):
+        # 根据给定的方法计算注意力（能量）
+        # 三种计算attention的方式
+        if self.method == 'general': # softmax(sum(hidden*(Wa × encoder_output),2))
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        elif self.method == 'concat': # 将当前隐层与每个encoder输出进行concat，然后经过fc处理，再用softmax计算权重(可以参考Andrew ng在序列模型课程第三周中的讲解)
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+        elif self.method == 'dot': # 比general方式少一层全连接，直接求softmax(sum(hidden*encoder_output,2))
+            attn_energies = self.dot_score(hidden, encoder_outputs)
+
+        # (max_length,batch_size) 转置为 (batch_size,max_length). 记得上面的转置处理吗，这里需要转置回来方便求权重
+        attn_energies = attn_energies.t()
+
+        # 返回权重信息，使用unsqueeze添加一个维度方便后续计算
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
+
+
+# 再定义decoder的计算图，与encoder结构类似，不过多了一层attention层
+class LuongAttnDecoderRNN(nn.Module):
+    def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1):
+        super(LuongAttnDecoderRNN, self).__init__()
+
+        self.attn_model = attn_model  #对应Attn的method
+        self.hidden_size = hidden_size #隐层大小
+        self.output_size = output_size #语料库大小,预测每个词的概率
+        self.n_layers = n_layers       #GRU层数
+        self.dropout = dropout
+
+        # 定义层
+        self.embedding = embedding
+        self.embedding_dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=(0 if n_layers == 1 else dropout))
+        self.concat = nn.Linear(hidden_size * 2, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+
+        self.attn = Attn(attn_model, hidden_size)
+
+    def forward(self, input_step, last_hidden, encoder_outputs):
+        """
+        Args:
+            input_step：上一time_step实际词语或上一time_step预测词语， shape = (1,batch_size)
+            last_hidden: GRU的最终隐藏层,作为gru的第二个参数，上面已经说过，shape =（n_layers x num_directions，batch_size，hidden_size）
+            encoder_outputs: shape =（max_length，batch_size，hidden_size）
+        """
+        # 注意：我们一次运行这一步（单词）
+        # 获取当前输入字的嵌入
+        embedded = self.embedding(input_step)
+        embedded = self.embedding_dropout(embedded)
+        # 通过单向GRU转发
+        rnn_output, hidden = self.gru(embedded, last_hidden)
+        # 从当前GRU输出计算注意力
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        # 将注意力权重乘以编码器输出以获得新的“加权和”上下文向量
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
+        # 使用Luong的公式五连接加权上下文向量和GRU输出
+        rnn_output = rnn_output.squeeze(0)
+        context = context.squeeze(1)
+        concat_input = torch.cat((rnn_output, context), 1)
+        concat_output = torch.tanh(self.concat(concat_input))
+        # 使用Luong的公式6预测下一个单词
+        output = self.out(concat_output)
+        output = F.softmax(output, dim=1)
+        # 返回输出和在最终隐藏状态
+        return output, hidden
+
+
+# 训练
+
+# masked损失
+def maskNLLLoss(inp, target, mask):
+    nTotal = mask.sum()
+
+    # torch.gather(inp, 1, target.view(-1, 1))这句代码相当于把真实的下一个词在预测中的概率取出
+    # 整句就是y(log(h(x)))求交叉熵
+    crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)).squeeze(1))
+
+    # 只计算原句有词部分的误差
+    loss = crossEntropy.masked_select(mask).mean()
+    loss = loss.to(device)
+    return loss, nTotal.item()
+
+"""
+操作顺序
+1.通过编码器前向计算整个批次输入。
+2.将解码器输入初始化为SOS_token，将隐藏状态初始化为编码器的最终隐藏状态。
+3.通过解码器一次一步地前向计算输入一批序列。
+4.如果是 teacher forcing 算法：将下一个解码器输入设置为当前目标;如果是 no teacher forcing 算法：将下一个解码器输入设置为当前解码器输出。
+5.计算并累积损失。
+6.执行反向传播。
+7.裁剪梯度。
+8.更新编码器和解码器模型参数。
+PyTorch的RNN模块（RNN，LSTM，GRU）可以像任何其他非重复层一样使用，只需将整个输入序列（或一批序列）传递给它们。
+ 我们在编码器中使用GRU层就是这样的。实际情况是，在计算中有一个迭代过程循环计算隐藏状态的每一步。
+ 或者，你每次只运行一个 模块。在这种情况下，我们在训练过程中手动循环遍历序列就像我们必须为解码器模型做的那样。
+ 只要你正确的维护这些模型的模块，就 可以非常简单的实现顺序模型。
+"""
+def train(input_variable, lengths, target_variable, mask, max_target_len, encoder, decoder, embedding,
+          encoder_optimizer, decoder_optimizer, batch_size, clip, max_length=MAX_LENGTH, teacher_forcing_ratio = 1.0):
+
+    # 零化梯度
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    # 设置设备选项
+    input_variable = input_variable.to(device)
+    lengths = lengths.to(device)
+    target_variable = target_variable.to(device)
+    mask = mask.to(device)
+
+    # 初始化变量
+    loss = 0
+    print_losses = []
+    n_totals = 0
+
+    # 正向传递编码器
+    encoder_outputs, encoder_hidden = encoder(input_variable, lengths)
+
+    # 创建初始解码器输入（从每个句子的SOS令牌开始）
+    decoder_input = torch.LongTensor([[SOS_token for _ in range(batch_size)]])
+    decoder_input = decoder_input.to(device)
+
+    # 将初始解码器隐藏状态设置为编码器的最终隐藏状态
+    decoder_hidden = encoder_hidden[:decoder.n_layers]
+
+    # 确定我们是否此次迭代使用`teacher forcing`
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    # 通过解码器一次一步地转发一批序列
+    if use_teacher_forcing:
+        for t in range(max_target_len):
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            # Teacher forcing: 下一个输入是当前的目标
+            decoder_input = target_variable[t].view(1, -1)
+            # 计算并累计损失
+            mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
+            loss += mask_loss
+            print_losses.append(mask_loss.item() * nTotal)
+            n_totals += nTotal
+    else:
+        for t in range(max_target_len):
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            # No teacher forcing: 下一个输入是解码器自己的当前输出
+            _, topi = decoder_output.topk(1)
+            decoder_input = torch.LongTensor([[topi[i][0] for i in range(batch_size)]])
+            decoder_input = decoder_input.to(device)
+            # 计算并累计损失
+            mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
+            loss += mask_loss
+            print_losses.append(mask_loss.item() * nTotal)
+            n_totals += nTotal
+
+    # 执行反向传播
+    loss.backward()
+
+    # 剪辑梯度：梯度被修改到位
+    _ = torch.nn.utils.clip_grad_norm_(encoder.parameters(), clip)
+    _ = torch.nn.utils.clip_grad_norm_(decoder.parameters(), clip)
+
+    # 调整模型权重
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return sum(print_losses) / n_totals
+
+def trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_optimizer, embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size, print_every, save_every, clip, corpus_name, loadFilename):
+    
+    # 为每次迭代加载batches
+    training_batches = [batch2TrainData(voc, [random.choice(pairs) for _ in range(batch_size)])
+                      for _ in range(n_iteration)]
+
+    # 初始化
+    print('Initializing ...')
+    start_iteration = 1
+    print_loss = 0
+    if loadFilename:
+        start_iteration = checkpoint['iteration'] + 1
+
+    # 训练循环
+    print("Training...")
+    for iteration in range(start_iteration, n_iteration + 1):
+        training_batch = training_batches[iteration - 1]
+        # 从batch中提取字段
+        input_variable, lengths, target_variable, mask, max_target_len = training_batch
+
+        # 使用batch运行训练迭代
+        loss = train(input_variable, lengths, target_variable, mask, max_target_len, encoder,
+                     decoder, embedding, encoder_optimizer, decoder_optimizer, batch_size, clip)
+        print_loss += loss
+
+        # 打印进度
+        if iteration % print_every == 0:
+            print_loss_avg = print_loss / print_every
+            print("Iteration: {}; Percent complete: {:.1f}%; Average loss: {:.4f}".format(iteration, iteration / n_iteration * 100, print_loss_avg))
+            print_loss = 0
+
+        # 保存checkpoint
+        if (iteration % save_every == 0):
+            directory = os.path.join(save_dir, model_name, corpus_name, '{}-{}_{}'.format(encoder_n_layers, decoder_n_layers, hidden_size))
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            torch.save({
+                'iteration': iteration,
+                'en': encoder.state_dict(),
+                'de': decoder.state_dict(),
+                'en_opt': encoder_optimizer.state_dict(),
+                'de_opt': decoder_optimizer.state_dict(),
+                'loss': loss,
+                'voc_dict': voc.__dict__,
+                'embedding': embedding.state_dict()
+            }, os.path.join(directory, '{}_{}.tar'.format(iteration, 'checkpoint')))
+
+
+# 评估时和真正对话时的解码类，此处不再需要训练，也没有真实的target，于是使用decoder每个time_step预测可能性最大的词作为下一个time_step的输入
+class GreedySearchDecoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(GreedySearchDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, input_seq, input_length, max_length):
+        # 通过编码器模型转发输入
+        encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
+        # 准备编码器的最终隐藏层作为解码器的第一个隐藏输入
+        decoder_hidden = encoder_hidden[:decoder.n_layers]
+        # 使用SOS_token初始化解码器输入
+        decoder_input = torch.ones(1, 1, device=device, dtype=torch.long) * SOS_token
+        # 初始化张量以将解码后的单词附加到
+        all_tokens = torch.zeros([0], device=device, dtype=torch.long)
+        all_scores = torch.zeros([0], device=device)
+        # 一次迭代地解码一个词tokens
+        for _ in range(max_length):
+            # 正向通过解码器
+            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden, encoder_outputs)
+            # 获得最可能的单词标记及其softmax分数
+            decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
+            # 记录token和分数
+            all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
+            all_scores = torch.cat((all_scores, decoder_scores), dim=0)
+            # 准备当前令牌作为下一个解码器输入（添加维度）
+            decoder_input = torch.unsqueeze(decoder_input, 0)
+        # 返回收集到的词tokens和分数
+        return all_tokens, all_scores
+
+# 构建评估和预测方法，实际上评估和真正对话时我们的输入句子大概率只有1个，但我们还是沿用我们已经实现的数据处理方法将它改造成batch_size=1的标量从而输入模型
+def evaluate(encoder, decoder, searcher, voc, sentence, max_length=MAX_LENGTH):
+    ### 格式化输入句子作为batch
+    # words -> indexes
+    indexes_batch = [indexesFromSentence(voc, sentence)]
+    # 创建lengths张量
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    # 转置batch的维度以匹配模型的期望
+    input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
+    # 使用合适的设备
+    input_batch = input_batch.to(device)
+    lengths = lengths.to(device)
+    # 用searcher解码句子
+    tokens, scores = searcher(input_batch, lengths, max_length)
+    # indexes -> words
+    decoded_words = [voc.index2word[token.item()] for token in tokens]
+    return decoded_words
+
+# 用户接口，简易对话窗口
+def evaluateInput(encoder, decoder, searcher, voc):
+    input_sentence = ''
+    while(1):
+        try:
+            # 获取输入句子
+            input_sentence = input('> ')
+            # 检查是否退出
+            if input_sentence == 'q' or input_sentence == 'quit': break
+            # 规范化句子
+            input_sentence = unicodeToAscii(input_sentence)
+            # 评估句子
+            output_words = evaluate(encoder, decoder, searcher, voc, input_sentence)
+            # 格式化和打印回复句
+            output_words[:] = [x for x in output_words if not (x == 'EOS' or x == 'PAD')]
+            print('Bot:', ' '.join(output_words))
+
+        # 输入的词不再词汇表中则会报此错误 todo..添加unknown标记
+        except KeyError:
+            print("Error: Encountered unknown word.")
+
+# 配置模型
+model_name = 'cb_model'
+attn_model = 'dot'
+#attn_model = 'general'
+#attn_model = 'concat'
+hidden_size = 500
+encoder_n_layers = 2
+decoder_n_layers = 2
+dropout = 0.1
+batch_size = 64
+
+# 设置检查点以加载; 如果从头开始，则设置为None
+# loadFilename = './data/4000_checkpoint.tar'
+loadFilename = None
+checkpoint_iter = 4000
+#loadFilename = os.path.join(save_dir, model_name, corpus_name,
+#                            '{}-{}_{}'.format(encoder_n_layers, decoder_n_layers, hidden_size),
+#                            '{}_checkpoint.tar'.format(checkpoint_iter))
+
+# 如果提供了loadFilename，则加载模型
+if loadFilename:
+    # 如果在同一台机器上加载，则对模型进行训练
+    checkpoint = torch.load(loadFilename)
+    # If loading a model trained on GPU to CPU
+    #checkpoint = torch.load(loadFilename, map_location=torch.device('cpu'))
+    encoder_sd = checkpoint['en']
+    decoder_sd = checkpoint['de']
+    encoder_optimizer_sd = checkpoint['en_opt']
+    decoder_optimizer_sd = checkpoint['de_opt']
+    embedding_sd = checkpoint['embedding']
+    voc.__dict__ = checkpoint['voc_dict']
+
+print('Building encoder and decoder ...')
+# 初始化词向量
+embedding = nn.Embedding(voc.num_words, hidden_size)
+if loadFilename:
+    embedding.load_state_dict(embedding_sd)
+# 初始化编码器 & 解码器模型
+encoder = EncoderRNN(hidden_size, embedding, encoder_n_layers, dropout)
+decoder = LuongAttnDecoderRNN(attn_model, embedding, hidden_size, voc.num_words, decoder_n_layers, dropout)
+if loadFilename:
+    encoder.load_state_dict(encoder_sd)
+    decoder.load_state_dict(decoder_sd)
+# 使用合适的设备
+encoder = encoder.to(device)
+decoder = decoder.to(device)
+print('Models built and ready to go!')
+
+# 配置训练/优化
+clip = 50.0
+teacher_forcing_ratio = 1.0
+learning_rate = 0.0001
+decoder_learning_ratio = 5.0
+n_iteration = 4000
+print_every = 1
+save_every = 500
+
+# 确保dropout layers在训练模型中
+encoder.train()
+decoder.train()
+
+# 初始化优化器
+print('Building optimizers ...')
+encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate * decoder_learning_ratio)
+if loadFilename:
+    encoder_optimizer.load_state_dict(encoder_optimizer_sd)
+    decoder_optimizer.load_state_dict(decoder_optimizer_sd)
+
+def startChat():
+    # 将dropout layers设置为eval模式 
+    encoder.eval()
+    decoder.eval()
+
+    # 初始化探索模块
+    searcher = GreedySearchDecoder(encoder, decoder)
+
+    # 开始聊天（取消注释并运行以下行开始）
+    evaluateInput(encoder, decoder, searcher, voc)
+
+# 运行训练迭代
+# if not loadFilename:
+print("Starting Training!")
+trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_optimizer,
+        embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size,
+        print_every, save_every, clip, corpus_name, loadFilename)
+# else:
+startChat()
+
+
